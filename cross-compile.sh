98596926cc205a09549d07be400a7998f46df392
Zach Knight <aeros.storkpk@gmail.com>
2018-06-17 08:12:33 -0500

    Rewrote the cross compiler script

--- cross-compile.sh
+++ cross-compile.sh
@@ -1,35 +1,67 @@
 #!/bin/bash
 
-# CLEAN ALL TARGET BINS
-echo -n 'Cleaning old binaries...'
-find ./bin -iname 'rscgo*' -delete
-echo 'done'
+# Set the name of the binary file to create, global var
+BINARY_NAME='GoPK'
 
-# BUILD NEW TARGET BINS, LOTS OF THEM
-echo -n 'Building binaries for OpenBSD...'
-GOARCH=amd64 GOOS=openbsd go build -o bin/target/openbsd/64/rscgo
-GOARCH=386 GOOS=openbsd go build -o bin/target/openbsd/32/rscgo
-echo 'done'
-echo -n 'Building binaries for Darwin/MacOSX...'
-GOARCH=amd64 GOOS=darwin go build -o bin/target/darwin/64/rscgo
-GOARCH=386 GOOS=darwin go build -o bin/target/darwin/32/rscgo
-echo 'done'
-echo -n 'Building binaries for NetBSD...'
-GOARCH=amd64 GOOS=netbsd go build -o bin/target/netbsd/64/rscgo
-GOARCH=386 GOOS=netbsd go build -o bin/target/netbsd/32/rscgo
-echo 'done'
-echo -n 'Building binaries for FreeBSD...'
-GOARCH=amd64 GOOS=freebsd go build -o bin/target/freebsd/64/rscgo
-GOARCH=386 GOOS=freebsd go build -o bin/target/freebsd/32/rscgo
-echo 'done'
-echo -n 'Building binaries for Windows...'
-GOARCH=amd64 GOOS=windows go build -o bin/target/windows/64/rscgo.exe
-GOARCH=386 GOOS=windows go build -o bin/target/windows/32/rscgo.exe
-echo 'done'
-echo -n 'Building binaries for Linux...'
-GOARCH=amd64 GOOS=linux go build -o bin/target/linux/64/rscgo
-GOARCH=386 GOOS=linux go build -o bin/target/linux/32/rscgo
-echo 'done'
-echo -n 'Building binaries for (Open)(Solaris|Indiana) (amd64 only)...'
-GOARCH=amd64 GOOS=solaris go build -o bin/target/solaris/64/rscgo
-echo 'done'
+
+# Clean the old binaries.  Might be unnecessary, not sure.
+# Unsure if `find` args are POSIX compliant or not, maybe look into it later and if not fix it.
+function cleanBinaries() {
+    echo -n "Cleaning old $BINARY_NAME binaries..."
+    find ./bin/ -executable -xtype f -delete
+    echo 'done'
+}
+
+# Build new binaries.  Takes 2 arguments.
+#
+# arg1(arch): the target platforms architecture
+# arg2(os): the target platforms operating system
+function buildBinaries() {
+    local arch=$1
+    local os=$2
+    local shortArch='64'
+    local binSuffix=''
+
+    if [[ $arch = 'amd64' ]]; then
+        shortArch='64'
+    else
+        shortArch='32'
+    fi
+
+    if [[ $os = 'windows' ]]; then
+        binSuffix='.exe'
+    fi
+
+    echo -n "Building $BINARY_NAME for $os/$shortArch (dest='./bin/target/$os/$shortArch/$BINARY_NAME$binSuffix')..."
+    GOARCH=$arch GOOS=$os go build -o "./bin/target/$os/$shortArch/$BINARY_NAME$binSuffix"
+    echo 'done'
+}
+
+if [[ $# == 0 ]]; then
+	# clean house
+	cleanBinaries
+
+	# build house
+	for os in linux openbsd freebsd netbsd darwin solaris windows; do
+	    if [[ $os != 'solaris' ]]; then
+	        buildBinaries 386 $os
+	    fi
+	    buildBinaries amd64 $os
+	    echo
+	done
+
+	echo "Compilation for all of the platforms has completed.  Goodbye."
+elif [[ $1 = 'clean' ]]; then
+	cleanBinaries
+elif [[ $1 = 'make' ]]; then
+	# build house
+	for os in linux openbsd freebsd netbsd darwin solaris windows; do
+	    if [[ $os != 'solaris' ]]; then
+	        buildBinaries 386 $os
+	    fi
+	    buildBinaries amd64 $os
+	    echo
+	done
+
+	echo "Compilation for all of the platforms has completed.  Goodbye."
+fi
